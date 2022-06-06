6ec7b368d05c62210d18c9d5c9fbf6eac1ad6f54
PeterL <peter@ourgaff.com>
2022-06-06 12:31:03 +0100

    jacoco move fix

--- src/main/java/com/tsi/pedro/Project/Actor.java
+++ src/main/java/com/tsi/pedro/Project/Actor.java
@@ -19,7 +19,7 @@ public class Actor {
     private String first_name;
     private String last_name;
 
-    public Actor(String first_name,String last_name){
+    public Actor(String first_name, String last_name){
         this.first_name = first_name;
         this.last_name = last_name;
     }
