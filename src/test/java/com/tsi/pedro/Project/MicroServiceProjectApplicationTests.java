890e6d5c65ac05541247ce8015c2bf16774db535
PeterL <peter@ourgaff.com>
2022-06-06 11:59:08 +0100

    jacoco 5

--- src/test/java/com/tsi/pedro/Project/MicroServiceProjectApplicationTests.java
+++ src/test/java/com/tsi/pedro/Project/MicroServiceProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.tsi.pedro.Project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MicroServiceProjectApplicationTests {
 
 	@Test
